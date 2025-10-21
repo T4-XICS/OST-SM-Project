d38a0fe9c0358e4429e48c6307a2f3c085f3ce32
maleczlevente <maleczlevente@stud.uni-obuda.hu>
2025-10-21 14:00:20 +0200

    misc

--- consumer/kafka_spark_stream.py
+++ consumer/kafka_spark_stream.py
@@ -2,20 +2,18 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StringType, TimestampType
 
-# 1. Spark session
+#session
 spark = SparkSession.builder \
     .appName("ICS-Stream-Consumer") \
     .getOrCreate()
 
 spark.sparkContext.setLogLevel("WARN")
 
-# 2. Schema (adapt based on your CSV header!)
 schema = StructType() \
     .add("timestamp", StringType()) \
     .add("LIT101", StringType()) \
     .add("FIT101", StringType()) 
 
-# 3. Read from Kafka topic
 df_kafka_raw = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "kafka:9092") \
@@ -23,12 +21,10 @@ df_kafka_raw = spark.readStream \
     .option("startingOffsets", "latest") \
     .load()
 
-# 4. Decode Kafka value field
 df_json = df_kafka_raw.selectExpr("CAST(value AS STRING)")
 
 df_parsed = df_json.select(from_json(col("value"), schema).alias("data")).select("data.*")
 
-# 5. Output to console for now
 query = df_parsed.writeStream \
     .outputMode("append") \
     .format("console") \
